6962edb1481b1b586e15f4e5d2d6b4b9ade84a72
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-09-20 17:14:49 +0700

    template layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
 import Breadcrumbs from './components/layouts/breadcrumbs';
+import Footer from './components/layouts/footer';
 import Header from './components/layouts/header';
 import Hero from './components/layouts/hero';
+import Sidebar from './components/layouts/sidebar';
 
 function App() {
   
@@ -10,6 +12,19 @@ function App() {
         <Header />
         <Hero/>
         <Breadcrumbs/>
+        <section className="product spad">
+          <div className="container">
+              <div className="row">
+                <div className="col-lg-3 col-md-5">
+                    <Sidebar/>
+                </div>
+                <div className="col-lg-9 col-md-7">
+
+                </div>
+              </div>
+            </div>
+          </section>
+          <Footer/>
     </div>
   );
 }
